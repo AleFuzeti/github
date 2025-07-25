98f236bc0bb9e195049d82f846650d069756b41a
AleFuzeti <alebertipaglia@gmail.com>
2025-07-25 08:01:28 -0300

    Deploy portfolio with fixed asset paths for GitHub Pages

--- _next/static/ixDh6QuHCHxlDNTbVzmHX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-d1e2d45efcac62d1.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
